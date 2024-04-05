ddc2592c4a209bde719fc13f903e785b3bd21f97
unknown <panditdipesh12@gmail.com>
2024-04-05 12:19:01 +0545

    updated not found page

--- src/components/pages/NotFound/NotFoundPage.js
+++ src/components/pages/NotFound/NotFoundPage.js
@@ -1,5 +1,12 @@
+import './NotFoundPage.css';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default function NotFoundPage(){
     return(
-        <>Page not found</>
-    )
-}
+        <div className='container not-found'>
+            <FontAwesomeIcon icon={faExclamationTriangle} size='3x'/>
+            <h2>Page Not found</h2>
+        </div>
+    );
+};
